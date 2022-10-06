5a12fa4db0e67c7b011638cdb1c6524ea878cb7f
koi151 <111631682+koi151@users.noreply.github.com>
2022-10-06 09:30:00 +0700

    Update (5) So Xuat Hien Nhieu Nhat Trong Mang.cpp

--- Exercise/(5) So Xuat Hien Nhieu Nhat Trong Mang.cpp
+++ Exercise/(5) So Xuat Hien Nhieu Nhat Trong Mang.cpp
@@ -32,15 +32,16 @@ using namespace std;
 //========================================================
 int main (){
     FAST_IO;
-    int n, val, res = 1, cnt = 1;
+    int n; 
     cin >> n;
     int a[n];
     for (auto &x : a)
         cin >> x;
     sort(a, a+n);
 
-    for (int i = 1; i < n; i ++){
-        if (a[i] == a[i-1])
+    int val = a[0], res = 1, cnt = 1;
+    for (int i = 0; i < n-1; i ++){
+        if (a[i] == a[i+1])
             cnt ++;
         else cnt = 1;
         if (cnt > res){
